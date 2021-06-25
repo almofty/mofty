de3bb65898887a001070f25552426602d86dcf65
almofty <86256600+almofty@users.noreply.github.com>
2021-06-26 01:03:57 +0200

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -2698,8 +2698,7 @@ keyboard.inline_keyboard = {
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-end
-]]
+end]].
 keyboard = {} 
 keyboard.inline_keyboard = {
 }
