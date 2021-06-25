e586cbbd83f3142da06578af3f1c41e82fb7d40d
almofty <86256600+almofty@users.noreply.github.com>
2021-06-26 01:01:14 +0200

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -2699,10 +2699,6 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
-
-if text == 'هيكتور ' or text == 'HECTOR' or text == 'HECTOR' then
-local Text = [[
-DEV HECTOR
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
